b460c16efd13f37f0b24ddc0b5882458174c9668
nitkan7 <57367051+nitkan7@users.noreply.github.com>
2022-03-18 23:59:12 +0530

    Update simple-fiber-usage.go

--- simple-fiber-usage.go
+++ simple-fiber-usage.go
@@ -2,10 +2,12 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+
+
 func main(){
-app:=fiber.New()
+app:=fiber.New() //creates a new fiber instance 
 
-
+//app.Method
 app.Get("/",func(c *fiber.Ctx)error{
 return c.SendString("Hello, World!")
 })
